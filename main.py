1e8373ebc4f0315635a4d775f3b918f6fbc3d1f2
isitusnoamow <240135@hkis.edu.hk>
2022-10-30 16:11:58 +0800

    all buttons working

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ zButton = Button(2)
 zCode = NULL_CHAR
 xButton = Button(3)
 xCode = NULL_CHAR
+wButton = Button(4)
+wCode = NULL_CHAR
+aButton = Button(14)
+aCode = NULL_CHAR
+sButton = Button(15)
+sCode = NULL_CHAR
+dButton = Button(18)
+dCode = NULL_CHAR
 
 
 def write_report(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
-    
+
 while True:
     if zButton.is_pressed:
         zCode = chr(29)
@@ -23,4 +31,24 @@ while True:
     else:
         xCode = NULL_CHAR
 
-    write_report(NULL_CHAR*2+zCode+xCode+NULL_CHAR*4)
+    if wButton.is_pressed:
+        wCode = chr(26)
+    else:
+        wCode = NULL_CHAR
+
+    if aButton.is_pressed:
+        aCode = chr(4)
+    else:
+        aCode = NULL_CHAR
+
+    if sButton.is_pressed:
+        sCode = chr(22)
+    else:
+        sCode = NULL_CHAR
+
+    if dButton.is_pressed:
+        dCode = chr(7)
+    else:
+        dCode = NULL_CHAR
+
+    write_report(NULL_CHAR*2+zCode+xCode+wCode+aCode+sCode+dCode)
