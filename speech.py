f22b104b113ca0f857dc44579746599f2ce48ac6
isitusnoamow <240135@hkis.edu.hk>
2022-10-30 18:54:18 +0800

    last function

--- speech.py
+++ speech.py
@@ -2,6 +2,10 @@ import pyaudio
 #mic at index 1
 import speech_recognition as sr
 
+def write_report(report):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(report.encode())
+
 r = sr.Recognizer()
 with sr.Microphone(device_index=1) as source:
     r.adjust_for_ambient_noise(source)
@@ -11,4 +15,6 @@ with sr.Microphone(device_index=1) as source:
             text = r.recognize_google(audio)
         except sr.UnknownValueError:
             print("fail")
-        print("I heard " + text)
+        print(text)
+        if "start" in text or "test" in text:
+            write_report(NULL_CHAR*2+chr(6)+NULL_CHAR*5)
